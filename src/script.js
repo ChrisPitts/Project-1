1780af2a59cbd4e01e85eb20016273a1a5d6277d
Chris Pitts <cp25393@gmail.com>
2022-11-11 09:44:52 -0600

    increased animation delay

--- src/script.js
+++ src/script.js
@@ -15,7 +15,7 @@ function onLoad(event){
         for(let i = 0; i < modules.length; i++){
             if(modules[i].isIntersecting){
                 setTimeout(function() {modules[i].target.classList.add('fadeup');}, timer);
-                timer += 100;
+                timer += 250;
                 intersectionObserver.unobserve(modules[i].target);
                 setTimeout(function() {timer = 0; }, 1000);
             }
